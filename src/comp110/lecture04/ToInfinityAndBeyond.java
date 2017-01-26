72d6dcbdc6113f13ec36b6c80a1573f89620dc0a
Kris Jordan <krisjordan@gmail.com>
2017-01-26 07:01:46 -0500

    Lecture 04 WIP

---
+++ src/comp110/lecture04/ToInfinityAndBeyond.java
@@ -0,0 +1,18 @@
+package comp110.lecture04;
+
+import comp110.Console;
+
+public class ToInfinityAndBeyond {
+
+	public static void main(String[] args) {
+
+		Console console = new Console("To Inifinity and Beyond");
+		console.speed(0.1);
+
+		while (true) {
+			console.print("OMG. I <3 U.");
+		}
+
+	}
+
+}
