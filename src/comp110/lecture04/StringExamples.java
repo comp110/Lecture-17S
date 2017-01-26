aec18d7ae81ac8ae80ce5aa3a4ea24cb10dc95e9
Kris Jordan <krisjordan@gmail.com>
2017-01-26 09:12:30 -0500

    Lecture 4 examples

--- src/comp110/lecture04/StringExamples.java
+++ src/comp110/lecture04/StringExamples.java
@@ -4,48 +4,20 @@ import comp110.Console;
 
 public class StringExamples {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		Console console = new Console("String Method Examples");
-		console.speed(0.6);
+    Console console = new Console("String Method Examples");
+    console.speed(0.6);
 
-		while (true) {
+    while (true) {
 
-			console.print("1. String's boolean Test Methods");
-			console.print("2. String's comparison Methods");
-			console.print("3. String's index-related Methods");
-			console.print("4. String manipulation Methods");
+      String choice = console.promptString("Input String");
 
-			int choice = console.promptInt("Your choice:");
+      // TODO: Example Method Calls
+      console.print(choice.toUpperCase());
 
-			if (choice == 1) {
-				console.print("hi".equals("hi"));
-				console.print("hi".equalsIgnoreCase("HI"));
-				console.print("hello world".contains("ello"));
-				console.print("hello".startsWith("hell"));
-				console.print("hello".endsWith("ello"));
-			} else {
-				if (choice == 2) {
-					console.print("abc".compareTo("xyz"));
-					console.print("abc".compareToIgnoreCase("ABC"));
-				} else {
-					if (choice == 3) {
-						console.print("the length of this string is".length());
-						console.print("hello world".substring(1, 8));
-						console.print("hello".charAt(3));
-						console.print("hello world".indexOf("o"));
-						console.print("hello world".lastIndexOf("o"));
-					} else {
-						console.print("one" + "concatenated with" + "another");
-						console.print("hehehehe".replace("e", "a"));
-						console.print("hehehehe".replaceAll("e", "a"));
-						console.print("chAnge Case uP".toUpperCase());
-						console.print("chAnge Case doWn".toLowerCase());
-					}
-				}
-			}
-		}
+    }
 
-	}
+  }
 
 }
