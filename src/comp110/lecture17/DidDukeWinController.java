46f360f52fd09452694c2a8c67d7419ccb054782
Kris Jordan <krisjordan@gmail.com>
2017-03-21 07:18:24 -0400

    Lecture 17 Materials

---
+++ src/comp110/lecture17/DidDukeWinController.java
@@ -0,0 +1,20 @@
+package comp110.lecture17;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
+public class DidDukeWinController {
+
+    // Fields
+    private boolean _revealed;
+
+    // Controller
+    public DidDukeWinController() {
+        _revealed = false;
+    }
+
+    // Methods
+
+}
---
+++ src/comp110/lecture17/TearsController.java
@@ -0,0 +1,24 @@
+package comp110.lecture17;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class TearsController {
+
+    // Fields
+    @FXML
+    private Group _group;
+
+    @FXML
+    private Slider _size;
+
+    private Circle _tear;
+
+    // Methods
+
+}
