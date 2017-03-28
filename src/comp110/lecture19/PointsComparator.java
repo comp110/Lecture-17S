05abc621492cee56d969d952b4de68f32d6cdc33
Kris Jordan <krisjordan@gmail.com>
2017-03-28 08:55:29 -0400

    Minor tweak to 19

--- src/comp110/lecture19/PointsComparator.java
+++ src/comp110/lecture19/PointsComparator.java
@@ -6,11 +6,11 @@ public class PointsComparator implements Comparator<Player> {
 
 	public int compare(Player a, Player b) {
 		if (a.getPoints() < b.getPoints()) {
-			return -1;
+			return -1; // A comes before B
 		} else if (a.getPoints() == b.getPoints()) {
-			return 0;
+			return 0; // Either way
 		} else {
-			return 1;
+			return 1; // A comes after B
 		}
 	}
 
